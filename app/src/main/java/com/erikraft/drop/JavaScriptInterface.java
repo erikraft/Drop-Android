a84967498988e7f2a148de299ee1fb7e14f8567b
erikraft <139592038+erikraft@users.noreply.github.com>
2026-02-03 15:38:00 -0300

    ✨｜Feat(android): add select fallback and native chat notifications

--- app/src/main/java/com/erikraft/drop/JavaScriptInterface.java
+++ app/src/main/java/com/erikraft/drop/JavaScriptInterface.java
@@ -218,6 +218,11 @@ public class JavaScriptInterface {
         context.resetUploadIntent();
     }
 
+    @JavascriptInterface
+    public void showChatNotification(final String title, final String body) {
+        context.showChatNotification(title, body);
+    }
+
     public static class FileHeader {
         private final String name;
         private final String mime;
